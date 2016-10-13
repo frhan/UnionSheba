f7bf88e636ab55f254815cb5d9bf354883d583ef
Farhan Faruque <russel.cse05@gmail.com>
2016-10-13 08:50:18 +0600

    sign in working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable
+         :recoverable, :rememberable, :trackable, :authentication_keys => [:username]
 
   belongs_to :union
 
   def email_required?
+    false
+  end
+
+  def email_changed?
     false
   end
 
