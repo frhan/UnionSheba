47ca5b6eb34e0d985033bb7c5451f88c7cd66b62
Farhan Faruque <russel.cse05@gmail.com>
2016-10-13 22:25:47 +0600

    add pdf bangla

--- app/controllers/citizens_controller.rb
+++ app/controllers/citizens_controller.rb
@@ -3,14 +3,33 @@ class CitizensController < InheritedResources::Base
 
   def index
     if user_signed_in?
-      @citizens = Citizen.find_by_union_id(current_user.union.id)
+      @citizens = Citizen.where(:union_id => current_user.union.id)
+    end
+  end
+
+  def show
+    @citizen = Citizen.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render :pdf => file_name,
+               :template => 'citizens/show.pdf.erb',
+               :layout => 'pdf.html.erb',
+               :disposition => 'attachment',
+               :show_as_html => params[:debug].present?
+      end
     end
   end
 
   private
 
-    def citizen_params
-      params.require(:citizen).permit(:name_in_eng, :name_in_bng, :fathers_name, :mothers_name, :village, :post, :post, :union_id, :spouse_name)
-    end
+  def citizen_params
+    params.require(:citizen).permit(:name_in_eng, :name_in_bng, :fathers_name,
+                                    :mothers_name, :village, :post, :post, :union_id, :spouse_name)
+  end
+
+  def file_name
+    'file_name'
+  end
 end
 
