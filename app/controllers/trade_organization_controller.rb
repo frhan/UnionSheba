32a032c19e9ed528473ef2de0b52f361d27644f5
Farhan Faruque <russel.cse05@gmail.com>
2016-10-13 13:31:34 +0600

    add trade organization

--- app/controllers/trade_organization_controller.rb
+++ app/controllers/trade_organization_controller.rb
@@ -1,2 +1,25 @@
 class TradeOrganizationController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def renew
+
+  end
+
+  private
+  def trade_organization_params
+    params.require(:trade_organization).permit(:name)
+  end
+
 end
