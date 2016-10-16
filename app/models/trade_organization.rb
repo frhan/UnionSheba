17a9a1c0dd8d76be6aee2367517c6fcf23c430a8
Farhan Faruque <russel.cse05@gmail.com>
2016-10-16 22:52:41 +0600

    trade license compelted

--- app/models/trade_organization.rb
+++ app/models/trade_organization.rb
@@ -3,5 +3,16 @@ class TradeOrganization < ActiveRecord::Base
   has_many :trade_licenses
   accepts_nested_attributes_for :trade_licenses,:allow_destroy => true
 
+  def address
+      self.village_name << ',' << self.upazilla_name << ',' << self.zilla_name
+  end
+
+  def fee
+    '২০০'
+  end
+
+  def license_deadline
+    '২০১৫'
+  end
 
 end
