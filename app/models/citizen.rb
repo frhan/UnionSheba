9d85769703ecbd1e163afbbcdc83f33a9d7790f2
Farhan Faruque <russel.cse05@gmail.com>
2016-12-21 01:27:05 +0600

    added citizen validations

--- app/models/citizen.rb
+++ app/models/citizen.rb
@@ -2,4 +2,6 @@ class Citizen < ActiveRecord::Base
   belongs_to :union
 
   #TODO: before save nid and birthid should be saved in english
+  validates :name_in_eng,:name_in_bng ,:fathers_name,:mothers_name,
+            :village,:post,:word_no, presence: true
 end
