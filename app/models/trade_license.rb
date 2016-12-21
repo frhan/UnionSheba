f4d8deced8e189193d713caa65aa4c95c449b5b4
Farhan Faruque <russel.cse05@gmail.com>
2016-12-21 23:35:23 +0600

    update for no fine or emaining fee

--- app/models/trade_license.rb
+++ app/models/trade_license.rb
@@ -31,7 +31,11 @@ class TradeLicense < ActiveRecord::Base
   end
 
   def total_fee
-      self.fine_fee + self.remaining_fee + self.license_fee
+    total_fee = 0
+    total_fee = total_fee + self.fine_fee if self.fine_fee.present?
+    total_fee = total_fee + self.self.remaining_fee if self.remaining_fee.present?
+    total_fee = total_fee + self.license_fee if self.license_fee.present?
+    total_fee
   end
 
   private
