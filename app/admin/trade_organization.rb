4cd1584f43da65b83bd14d5b42d957884e542fbf
Farhan Faruque <russel.cse05@gmail.com>
2016-12-23 19:01:03 +0600

    shring admin column size

--- app/admin/trade_organization.rb
+++ app/admin/trade_organization.rb
@@ -1,5 +1,18 @@
 ActiveAdmin.register TradeOrganization do
 
+  index do
+    selectable_column
+    id_column
+    column :enterprize_name_in_eng
+    column :owners_name_eng
+    column :license_no
+    column :union
+    column :status
+    column :created_at
+    column :updated_at
+    actions
+  end
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
