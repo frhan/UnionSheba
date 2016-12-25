dacd6dd304279111b958fc32765347b67127ddc0
Farhan Faruque <russel.cse05@gmail.com>
2016-12-25 22:49:56 +0600

    add forms

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -2,11 +2,11 @@ class FormsController < ApplicationController
   before_action :authenticate_user!
 
   def tax_or_rate_form
-    resp_to ('forms/tax_or_rate_form.pdf.erb')
+    resp_to ('tax_or_rate_form')
   end
 
   def others_form
-    resp_to ('forms/others_form.pdf.erb')
+    resp_to ('others_form')
   end
 
   private
@@ -14,16 +14,16 @@ class FormsController < ApplicationController
   def resp_to (form_name)
     respond_to do |format|
       format.pdf do
-        render :pdf => 'form',
-               :template => form_name,
+        render :pdf => form_name,
+               :template => 'forms/'<<form_name<<'.pdf.erb',
                :layout => 'pdf.html.erb',
                :disposition => 'attachment',
                page_size: 'A4',
                :show_as_html => params[:debug].present?,
-               margin:  {   top:               10,                     # default 10 (mm)
+               margin:  {   top:               20,                     # default 10 (mm)
                             bottom:            10,
-                            left:              10,
-                            right:             10 },
+                            left:              0,
+                            right:             5 },
                dpi:                            '300'
       end
     end
