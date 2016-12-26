877516a3ced57b77beed14a3e2930bd380f0de56
Farhan Faruque <russel.cse05@gmail.com>
2016-12-26 12:09:40 +0600

    caliberate text size

--- app/controllers/trade_organizations_controller.rb
+++ app/controllers/trade_organizations_controller.rb
@@ -31,7 +31,7 @@ class TradeOrganizationsController < ApplicationController
                :show_as_html => params[:debug].present?,
                margin:  {   top:               0,                     # default 10 (mm)
                             bottom:            0,
-                            left:              20,
+                            left:              17,
                             right:             0 },
                dpi:                            '300'
                #zoom: 1.17647
