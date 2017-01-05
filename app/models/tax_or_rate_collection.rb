94346ec0597ae709ef07012eea96bf51c1a9842e
Farhan Faruque <russel.cse05@gmail.com>
2017-01-05 20:35:45 +0600

    add bangla on pdf

--- app/models/tax_or_rate_collection.rb
+++ app/models/tax_or_rate_collection.rb
@@ -4,4 +4,21 @@ class TaxOrRateCollection < ActiveRecord::Base
 
   accepts_nested_attributes_for :collection_money,:allow_destroy => true
   belongs_to :union
+
+  def serial_no_bangla
+    bangla_number self.collection_money.serial_no.to_s
+  end
+
+  def fine_bangla
+    bangla_number self.collection_money.fine.to_s
+  end
+
+  def fee_bangla
+    bangla_number self.collection_money.fee.to_s
+  end
+
+  def total_bangla
+    bangla_number self.collection_money.total.to_s
+  end
+
 end
