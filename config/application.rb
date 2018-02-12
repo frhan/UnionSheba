677a8e29adbc2a84569265f410188d0d8583a8e4
Farhan Faruque <russel.cse05@gmail.com>
2018-02-12 23:57:20 +0600

    added trade license org name

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,11 @@ module Updata
     config.time_zone = 'Asia/Dhaka'
     config.active_record.default_timezone = :local
 
+    config.logger = Logger.new(STDOUT)
+    config.logger = Log4r::Logger.new("Application Log")
+
+    config.log_level = :debug
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
