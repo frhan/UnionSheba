a6b22e524bb1512683ad38749bf3022e297300f3
Farhan Faruque <russel.cse05@gmail.com>
2018-07-10 03:11:25 +0600

    validations

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,6 +2,8 @@ class Expense < ActiveRecord::Base
   belongs_to :expense_category
   belongs_to :union
 
+  validates :expense_money, presence: true
+
   def exp_category
     if self.expense_category.others?
       return self.other_category
