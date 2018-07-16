bfab7866ad3f07325d664a68e25747fc63888b34
Farhan Faruque <russel.cse05@gmail.com>
2018-07-16 18:16:35 +0600

    added warish no and tracking id code

--- app/models/warish.rb
+++ app/models/warish.rb
@@ -16,6 +16,8 @@ class Warish < ActiveRecord::Base
   accepts_nested_attributes_for :warish_relations, allow_destroy: true
   accepts_nested_attributes_for :image_attachment, allow_destroy: true
 
+  after_create :save_tracking_id,:save_warish_no
+
 
   def set_status(status)
     self.status = status
@@ -43,5 +45,26 @@ class Warish < ActiveRecord::Base
     @permanent_address ||= self.addresses.permanent(current_lang).first if self.addresses.permanent(current_lang).first.present?
     @permanent_address
   end
+
+  private
+
+  def save_warish_no
+    return if self.pending? || self.citizen_no.present?
+
+    warsh_no = Warish.where(union_id: self.union.id).count(:warish_no)
+    warsh_no = warsh_no + 1
+    warsh = "#{self.union.union_code}C#{current_year.to_s}#{warsh_no.to_s}"
+    self.update_attributes(:warish_no => warsh)
+  end
+
+
+  def save_tracking_id
+    return if self.active? || self.tracking_id.present?
+
+    trac_no = Warish.where(union_id: self.union.id).count(:tracking_id)
+    trac_id = "#{self.union.union_code}W#{current_year_month_day.to_s}#{trac_no.to_s}"
+    self.update_attributes(:tracking_id => trac_id)
+  end
+
 
 end
