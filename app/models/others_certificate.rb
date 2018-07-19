4f3a3b2f1368c4c0b12e67f3ba44e4a617f07689
Farhan Faruque <russel.cse05@gmail.com>
2018-07-19 15:00:47 +0600

    made changes for others certificate

--- app/models/others_certificate.rb
+++ app/models/others_certificate.rb
@@ -1,3 +1,18 @@
 class OthersCertificate < ActiveRecord::Base
+  include ApplicationHelper, UnionHelper
+
+  belongs_to :union
+
+  has_many :addresses, as: :addressable, dependent: :destroy
+  has_many :basic_infos, as: :infoable, dependent: :destroy
+  has_one :contact_address, as: :contactable, dependent: :destroy
+  has_one :citizen_basic, as: :basicable, dependent: :destroy
+  has_one :image_attachment, as: :attachable, dependent: :destroy
+
+  accepts_nested_attributes_for :addresses, allow_destroy: true
+  accepts_nested_attributes_for :basic_infos, allow_destroy: true
+  accepts_nested_attributes_for :contact_address, allow_destroy: true
+  accepts_nested_attributes_for :citizen_basic, allow_destroy: true
+  accepts_nested_attributes_for :image_attachment, allow_destroy: true
 
 end
--- app/models/union.rb
+++ app/models/union.rb
@@ -1,6 +1,7 @@
 class Union < ActiveRecord::Base
   belongs_to :upazila
   has_many :citizens
+  has_many :others_certificates
   has_many :trade_organizations
   has_many :collection_moneys
   has_many :tax_or_rate_collections
