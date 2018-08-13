f27b7473bf4cefdf0b1c3c8d48b97329b28d0a39
Farhan Faruque <russel.cse05@gmail.com>
2018-08-13 15:58:59 +0600

    added sender name

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -12,14 +12,39 @@ class Voucher < ActiveRecord::Base
     self.expenses.sum(:expense_money)
   end
 
+  def collection?
+    self.main_voucher_type == 'collection'
+  end
+
+  def expense?
+    self.main_voucher_type == 'expense'
+  end
+
   def remove
 
+    if collection? && self.collection_moneys.count > 1
+      rearrange_voucher
+      return
+    end
+
+    if expense? && self.expenses.count > 1
+      rearrange_voucher
+      return
+    end
+
+    self.update_attributes status: :deleted
   end
 
 
-  def rearrance_voucher
+  def rearrange_voucher
     vouchers = Voucher.where(union_id: self.union.id, main_voucher_type: self.main_voucher_type,
                   :created_at =>self.created_at.beginning_of_day..self.created_at.end_of_day, status: :active)
+    if vouchers.present?
+      vouchers.vouchers do |v,index|
+        v.update_attributes voucher_no: index
+      end
+    end
+
   end
 
 end
