70d71e8f354ca6424e41d2191eaceaed146466d6
Farhan Faruque <russel.cse05@gmail.com>
2018-08-14 15:06:52 +0600

    voucher issue

--- app/helpers/voucher_helper.rb
+++ app/helpers/voucher_helper.rb
@@ -7,9 +7,7 @@ module VoucherHelper
     voucher = voucher.first if voucher.present?
 
     if voucher.blank?
-      voucher_count = Voucher.where(union_id: self.union.id, main_voucher_type: main_type,
-                                    :created_at => Date.today.beginning_of_day..Date.today.end_of_day,
-                                    status: :active).count
+      voucher_count = Voucher.where(union_id: self.union.id, main_voucher_type: main_type, status: :active).count
 
       voucher = Voucher.create(voucher_no: voucher_count + 1, main_voucher_type: main_type,
                                voucher_type: v_type, union: self.union)
--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -20,28 +20,30 @@ class Voucher < ActiveRecord::Base
   end
 
   def remove
+    self.update_attributes status: :deleted
 
     if collection? && self.collection_moneys.count > 1
       rearrange_voucher
-      return
     end
 
     if expense? && self.expenses.count > 1
       rearrange_voucher
-      return
     end
 
-    self.update_attributes status: :deleted
   end
 
 
   def rearrange_voucher
     vouchers = Voucher.where(union_id: self.union.id, main_voucher_type: self.main_voucher_type,
                              :created_at => self.created_at.beginning_of_day..self.created_at.end_of_day,
-                             status: :active)
+                             status: :active).order("voucher_id asc")
+
+    yesterday = self.created_at.yesterday
+    count = Voucher.where(union_id: self.union.id, :created_at => yesterday.beginning_of_day..yesterday.end_of_day,
+                          main_voucher_type: self.main_voucher_type, status: :active).count
     if vouchers.present?
-      vouchers.vouchers do |v, index|
-        v.update_attributes voucher_no: index
+      vouchers.each do |v|
+        v.update_attributes voucher_no: count + 1
       end
     end
 
