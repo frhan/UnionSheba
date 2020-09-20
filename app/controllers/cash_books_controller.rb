3b5fe0ad3694547f050fb0e661ffaed8530abf7d
Farhan Faruque <russel.cse05@gmail.com>
2020-09-20 16:05:13 +0200

    fix cash book controller

--- app/controllers/cash_books_controller.rb
+++ app/controllers/cash_books_controller.rb
@@ -15,29 +15,39 @@ class CashBooksController < ApplicationController
                               created_at: @start_date.beginning_of_day..@start_date.end_of_day)
                        .order("voucher_id,id desc")
 
-    @initial_collections = current_user.collection_moneys
+    if(!(@start_date.month == 7 && @start_date.day == 1))
+      @initial_collections = current_user.collection_moneys
                                .where(status: :active,
-                                      created_at: first_day_fiscal_year.beginning_of_day..@start_date.yesterday.end_of_day)
+                                      created_at: fday_fiscal_year(@start_date)
+                                                      .beginning_of_day..@start_date
+                                                      .yesterday.end_of_day)
+    end
 
     @expenses = current_user.expenses
                     .where(status: :active,
                            created_at: @start_date.beginning_of_day..@start_date.end_of_day)
                     .order("id asc")
+    if(!(@start_date.month == 7 && @start_date.day == 1))
+      @inital_expenses = current_user.expenses.where(status: :active,
+                                                      created_at: fday_fiscal_year(@start_date)
+                                                                      .beginning_of_day..@start_date.yesterday.end_of_day)
+                                                                      .sum(:expense_money)
+    end
 
-    @inital_expenses = current_user.expenses.where(status: :active,
-                                                   created_at: first_day_fiscal_year.beginning_of_day..
-                                                       @start_date.yesterday.end_of_day).sum(:expense_money)
+    if !@initial_collections.blank?
+      total_fee = @initial_collections.sum(:fee)
+      total_fine = @initial_collections.sum(:fine)
+      total_remain = @initial_collections.sum(:remain)
+      @initial_collections = total_fee + total_fine + total_remain
+    end
 
-
-    total_fee = @initial_collections.sum(:fee)
-    total_fine = @initial_collections.sum(:fine)
-    total_remain = @initial_collections.sum(:remain)
-    @initial_collections = total_fee + total_fine + total_remain
+    @initial_collections = 0 if @initial_collections.blank?
+    @inital_expenses = 0 if @inital_expenses.blank?
 
     #todays_total_collection = @collections.sum(:fee) + @collections.sum(:fine) + @collections.sum(:remain)
 
     last_year_balance = 0
-    last_year_balances = current_user.balance_moneys.where(tax_year: current_fiscal_year - 1)
+    last_year_balances = current_user.balance_moneys.where(tax_year: cfiscal_year(@start_date) - 1)
     last_year_balance = last_year_balances.first.value if last_year_balances.present?
 
     @balance = @initial_collections - @inital_expenses + last_year_balance
